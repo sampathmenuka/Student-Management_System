37a4260e3852bf6e8096bcda80b0b7ff10d2e935
sampathmenuka <sampath.m.chandimal99@gmail.com>
2026-04-19 16:37:21 +0530

    Add Student model and controller for student management system

---
+++ studentmanagement-app/app/Http/Controllers/StudentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use App\Models\student;
+use Illuminate\View\View;
+
+class StudentController extends Controller
+{
+    //
+}
