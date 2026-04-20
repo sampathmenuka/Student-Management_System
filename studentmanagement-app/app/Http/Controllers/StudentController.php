8d1cc1fcadea1a85a3be77553eeaebf9f553ceeb
sampathmenuka <sampath.m.chandimal99@gmail.com>
2026-04-20 08:42:03 +0530

    feat: implement student CRUD functionality with layout and controller scaffolding

--- studentmanagement-app/app/Http/Controllers/StudentController.php
+++ studentmanagement-app/app/Http/Controllers/StudentController.php
@@ -10,5 +10,10 @@ use Illuminate\View\View;
 
 class StudentController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $students = Student::all();
+
+        return view('students.index', compact('students'));
+    }
 }
