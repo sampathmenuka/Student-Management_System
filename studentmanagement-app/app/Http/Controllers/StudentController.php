fa3e6dd2fa94427b92ef4d23de4ef0897c1ddf88
sampathmenuka <sampath.m.chandimal99@gmail.com>
2026-04-20 09:19:26 +0530

    feat: add StudentController and views for listing and creating students

--- studentmanagement-app/app/Http/Controllers/StudentController.php
+++ studentmanagement-app/app/Http/Controllers/StudentController.php
@@ -16,4 +16,16 @@ class StudentController extends Controller
 
         return view('students.index', compact('students'));
     }
+
+    public function create(): View {
+
+      return view('students.create');
+    }
+
+    public function store(Request $request): RedirectResponse {
+        
+        $input = $request->all();
+        Student :: create($input);
+        return redirect('students')->with('flash_message', 'Student Addedd!');
+    }
 }
