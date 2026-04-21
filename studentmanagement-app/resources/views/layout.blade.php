ace3ff4dde659130587045cf7e8824763019fbff
sampathmenuka <sampath.m.chandimal99@gmail.com>
2026-04-21 17:29:56 +0530

    feat: add main layout and student management CRUD view templates

--- studentmanagement-app/resources/views/layout.blade.php
+++ studentmanagement-app/resources/views/layout.blade.php
@@ -95,7 +95,7 @@ div.content {
                 <div class="sidebar">
                 <a class="active" href="{{ url('/') }}">Home</a>
                 <a href="{{ url('/students') }}">Student</a>
-                <a href="#contact">Teacher</a>
+                <a href="{{ url('/teachers') }}">Teacher</a>
                 <a href="#about">Enrollment</a>
                 <a href="#about">Payment</a>
                 </div>
